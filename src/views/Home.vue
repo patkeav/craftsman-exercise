342b359d13751f7dc53f592d68d7ccfe587a6882
Patrick the Ratrick <keavenyp@gmail.com>
2022-03-15 11:00:47 -0600

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Welcome to Patrick Keaveny's skills assessment for Craftsman+!</h1>
+    <p>I really enjoyed talking with Jess about Craftsman+, and had a lot of fun building the following app.</p>
+    <p>
+      Unfortunately, the Superhero API doesn't appear to work with Facebook anymore, so I went with the Books API from
+      Google.
+    </p>
+    <p>
+      I also filtered by Stephen King to keep the data set large enough to be meaningful, but not so large as to
+      adversely affect performance.
+    </p>
+    <p>You can see the completed list of Books (and requirements outlined in the Readme) at the button below.</p>
+    <router-link :to="{ name: 'Books' }" class="button">Take me there!</router-link>
   </div>
 </template>
 
@@ -9,7 +20,15 @@ import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'Home',
-  components: {
-  },
+  components: {},
 });
 </script>
+<style scoped>
+.home {
+  color: #fff;
+}
+.button {
+  margin: 15px auto;
+  display: block;
+}
+</style>
